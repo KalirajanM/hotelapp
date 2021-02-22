d43075abb689b3a0e1a54d2e71d9a9b4d6621cf8
kalirajan <kalirajan@bsol.com>
2021-02-22 15:31:34 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
@@ -11,6 +25,7 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
